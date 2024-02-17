b8ba88e2137f801d5872d6e5ab7b7f35b88bd03f
aditya109 <adikid1996@gmail.com>
2024-02-17 23:08:51 +0530

    a few more programs
    Co-authored-by: Aryan <aryankr2005@users.noreply.github.com>

--- main.py
+++ main.py
@@ -112,30 +112,344 @@ b. int(x) 4     ✔
 
 """
 
-i = (10, 23, 34, 45)
-# print(i.count(23))
-# print(i)
+# i = (10, 23, 34, 45)
+# # print(i.count(23))
+# # print(i)
 
-j = [10, 23, 65, 98, 5, 35, 67]
-print(j)
-j.insert(3,45)  # inserts element at given index
-      # clears the list
-print(j)
-j.append(34)    # appends element at the end of the list
-print(j)   # [10, 23, 65, 45, 98, 5, 35, 67, 34] 
-frequency_of_5 = j.count(5)
-print(frequency_of_5)
-print(j.index(5))
+# j = [10, 23, 65, 98, 5, 35, 67]
+# print(j)
+# j.insert(3,45)  # inserts element at given index
+#       # clears the list
+# print(j)
+# j.append(34)    # appends element at the end of the list
+# print(j)   # [10, 23, 65, 45, 98, 5, 35, 67, 34] 
+# frequency_of_5 = j.count(5)
+# print(frequency_of_5)
+# print(j.index(5))
 
-last_element = j.pop()
-print(j)
-print(last_element)
+# last_element = j.pop()
+# print(j)
+# print(last_element)
 
-j.remove(98)
-print(j)
+# j.remove(98)
+# print(j)
 
-j.reverse()
-print(j)
+# j.reverse()
+# print(j)
 
-j.sort(reverse=False) # ascending
-print(j)
+# j.sort(reverse=False) # ascending
+# print(j)
+
+
+# i = ("api", 1, "23", 'a', 10.43)
+
+# print(i[0])
+
+# map = {}
+# map["12"] = "twelve"
+# map["13"] = "Thirteen"
+
+# print(map)
+
+# for index in range(1, 11):
+#     print(index)
+
+
+
+# print numbers from 10 to 1
+
+#  for i in range(10,0,-1):
+  #      print(i)
+
+# take an input and print numbers from 1 to input in multiples of 2
+# e.g., 
+# Enter a number: 23
+# 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22
+# n = int(input("Enter a Number :"))
+# for i in range(1,n):
+#         i=i * 2
+#         print(i)
+
+# n = int(input("Enter a Number :"))
+# for i in range(2, n, 2):
+#         print(i)
+
+# take an input and print numbers from 1 to input as odd numbers
+# e.g., 
+# Enter a number: 23
+# 1, 3, 5, 7, 9, 11, ...., 23
+# n = int(input("Enter a Number :"))
+# for i in range(1, n+1, 2):
+#         print(i)
+
+
+
+#n = int(input("Enter a number: "))
+#i = 1
+#while i <= n:
+ #       print(i)
+ #       i+=2
+
+# n = int(input("enter a no. : "))
+# i = 2
+# while i<=n:
+#         print(i)
+#         i+=2
+
+# fibonacci sequence
+# n = 23
+# 0,1,1,2,3,5,8,13,21
+# first = 0, second = 1, third = first + second, first = second, second = third
+# first = 0 
+# second = 1 
+        
+# n = int(input("enter a limit:"))
+# first = 0
+# second = 1
+# print(first)
+# print(second)
+
+# while (first+second) <= n:
+#         third = first + second
+#         first = second
+#         second = third
+#         print(third)
+
+
+
+# tribonacci sequence
+# n = 23
+# 0, 1, 1, 2, 4, 7, 13
+# n = int(input("Enter a no. : "))  
+# first = 0
+# second = 1
+# third = 1
+# print(first) 
+# print(second)
+# print(third) 
+# while (first+second+third) <= n:
+#     fourth = first + second + third
+#     first = second
+#     second = third
+#     third = fourth
+#     print(fourth)   
+
+# write a program to display the sum of all odd number from 1 to 100
+
+# sum = 0
+# for i in range(1 , 100):
+#     if i % 2 == 0:
+#         continue
+#     else:
+#         sum = sum + i
+# print(int(sum))       
+
+# sum = 0
+# for i in range(1, 100, 2):
+#     sum += i
+# print(sum)
+
+
+
+
+# write a program that displays all numbers from 1 to 100 that are not divisible by 2 as well as 3
+# for i in range(1 , 100):
+#     if i % 2 != 0 and i % 3 != 0:
+#         print(i)
+
+# write a program to sort 3 numbers in descending order using if else statement
+        
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# c = int(input("enter a number: "))
+
+# # a is greatest a > b and a > c
+# # b is greatest b > a and b > c
+# greatest = 0
+# big = 0
+# small = 0
+
+# if a > b and a > c:
+#     greatest = a
+# elif b > a and b > c:
+#     greatest = b
+# else:
+#     greatest = c
+
+# if a < b and a < c:
+#     small = a
+# elif b < a and b < c:
+#     small = b
+# else:
+#     small = c
+
+# big = (a + b + c) - (greatest + small)
+
+# print(greatest, big, small)
+
+
+# write a program to count the total number of digits in the given number
+
+# number_of_digits = 0
+# n = int(input("Enter a no. :"))
+# while n != 0:
+#     number_of_digits += 1
+#     n = n // 10
+# print(number_of_digits)
+""" 
+-> n = 121234218923948
+n = n / 10 = 12123421892394
+n = n / 10 = 1212342189239
+n = n / 10 = 121234218923
+n = n / 10 = 12123421892
+....
+n = n / 10 = 1
+n = n / 10 = 0
+
+
+ """
+
+# write a program to sum of digits in the given number
+""" 
+n -> 2390812903
+-> 2+3+9+0+8+1+2+9+0+3=37
+n
+
+
+ """
+# r = 0
+# sum_of_digits = 0
+# n = int(input("Enter a no. :"))
+# while n!=0:
+#     r = n % 10
+#     sum_of_digits += r
+#     n = n // 10
+
+# print(sum_of_digits)
+
+
+
+# write a program to calculate the sum and average of first n natural numbers
+# sum = 0
+# average = 0
+# count = 0
+# n = int(input("Enter a no.: "))
+# for i in range(1,n+1):
+#     sum += i
+# print(sum)
+# average = sum / n
+# print(average)
+
+
+
+""" 
+write a program to calculate grade of a student:
+
+100 - 91 -> grade A
+81 - 90  -> grade B
+71 - 80  -> grade C
+61 - 70  -> grade D
+51 - 60  -> grade E
+<= 50    -> grade F
+
+"""
+# marks = float(input("enter marks of student:"))
+# if marks <= 100 and marks >=91:
+#     print("Grade A")
+# elif marks <= 90 and marks >=81:
+#     print("Grade B")
+# elif marks <= 80 and marks >=71:
+#     print("Grade C")  
+# elif marks <= 70 and marks >=61:
+#     print("Grade D") 
+# elif marks <= 60 and marks >=51:
+#     print("Grade E") 
+# else:
+#     print("Grade F")    
+
+
+""" 
+*
+* *
+* * *
+* * * *
+* * * * *
+ """
+
+# for i in range(1,6): 
+#     for j in range (1, i+1):
+#         print("* ", end="")
+#     print()
+    
+""" 
+1
+1 2 
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+# for i in range(1 , 6):
+#         for j in range(1 , i+1):
+#                 print(j, " ", end="")
+#         print()
+        
+
+
+
+
+
+""" 
+1 2 3 4 5
+1 2 3 4 
+1 2 3 
+1 2
+1 
+
+"""
+# for i in range(6 , 1 , -1):
+#     for j in range(1 , i):
+#         print(j, " ", end="")
+#     print()      
+
+
+
+# WAP to swap two variables
+# a -> 10
+# b -> 12
+# a = 12, b = 10 
+# a , b = 12 , 10
+# c , d = 0 , 0
+# c = a 
+# d = b
+# a = (a + b) - a 
+# b = c
+# print("a =", a)
+# print("b =", b)
+
+# b = a + b
+# a = b - a
+# b = b - a 
+# print("a =", a)
+# print("b =", b)
+
+# wap to decimal to binary, octal and hexadecimal:
+binary = ""
+
+n = 123
+while n != 0:
+        bit = n % 2
+        binary = str(bit) + binary
+        n //= 2
+print(binary)
+
+
+""" 
+123 % 2 = 1; n = 123//2 = 61                     binary = "1"
+61 % 2 = 1; n = 61 // 2 = 30                                    binary = "1" + "1" = "11"
+30 % 2 = 0; n = 30 //2 = 15                                     binary = "0" + "11" = "011"
+15 % 2 = 1; n = 15 // 2 = 7                                      binary = "1" + "011" = "1011"
+7 % 2 = 1; n = 7 // 2 = 3                                                       binary = "1" + "1011" = "11011"
+3 % 2 = 1; n = 3 // 2 = 1                                                      binary = "1" + "11011"= "111011"
+1 % 2 = 1; n = 1 // 2 = 0===+                                                     binary = "1" + "111011" = "1111011"
+"""
